626fe32f3d03f51c834f80b256810b0f7129e35a
AMIR <55827972+amirhosseinbahrami0@users.noreply.github.com>
2024-12-20 14:06:03 +0330

    modified ADC

--- Fan/Fan/adc.c
+++ Fan/Fan/adc.c
@@ -4,3 +4,102 @@
  * Created: 12/20/2024 12:31:28 PM
  *  Author: Amir
  */ 
+#include "avr/io.h" /* To use the ADC Registers */
+#include "adc.h"
+#include "common_macros.h" /* To use the macros like SET_BIT */
+#include <avr/interrupt.h> /* For Timer ISR */
+
+
+
+
+/*******************************************************************************
+ *                          Global Variables                                   *
+ *******************************************************************************/
+/* Global volatile variable used in case interrupt mode is enabled */
+volatile uint16 g_adcResult = 0;
+/* Global volatile variable flag used for Lm-Sensor in case interrupt mode is enabled */
+volatile uint8 g_lmsensor;
+
+
+
+/*******************************************************************************
+ *                          ISR's Definitions                                  *
+ *******************************************************************************/
+/*
+ * Interrupt Service Routine Function if interrupt is enable for ADC.
+ */
+ISR(ADC_vect)
+{
+	/* Read ADC Data after conversion complete */
+	g_adcResult = ADC;
+}
+
+
+
+
+/*******************************************************************************
+ *                      Functions Definitions                                  *
+ *******************************************************************************/
+void ADC_init(const adc_config * Config_Ptr)
+{
+	/* ADMUX Register Bits Description:
+	 * REFS1:0 = 00 to choose to connect external reference voltage by input this voltage through AREF pin
+	 * ADLAR   = 0 right adjusted
+	 * MUX4:0  = 00000 to choose channel 0 as initialization
+	 */
+	ADMUX |= (Config_Ptr->vref) | (Config_Ptr->adalr) ;
+
+	/* ADCSRA Register Bits Description:
+	 * ADEN    = 1 Enable ADC
+	 * ADIE    = 0 Disable ADC Interrupt
+	 * ADATE   = 0 Disable Auto Trigger
+	 * ADPS2:0 = 011 to choose ADC_Clock=F_CPU/8=1Mhz/8=125Khz --> ADC must operate in range 50-200Khz
+	 */
+	ADCSRA = (Config_Ptr->enable) | (Config_Ptr->mode) | (Config_Ptr->prescaler) | (Config_Ptr->autotrigger);
+	/* Global volatile varibale flag set in case interrupt mode is enabled for Lm-Sensor */
+	if(((Config_Ptr->mode)==INTERRUPT_MODE))
+	{
+		g_lmsensor=1;
+	}
+	else
+	{
+		g_lmsensor=0;
+	}
+}
+
+
+/*
+ * Description :
+ * Function responsible for read analog data from a certain ADC channel in case of pooling mode.
+ * and convert it to digital using the ADC driver.
+ */
+uint16 ADC_readChannel_pooling(ADMUX_SEL channel_num)
+{
+	channel_num &= 0x07; /* Input channel number must be from 0 --> 7 */
+	ADMUX &= 0xE0; /* Clear first 5 bits in the ADMUX (channel number MUX4:0 bits) before set the required channel */
+	ADMUX = ADMUX | channel_num; /* Choose the correct channel by setting the channel number in MUX4:0 bits */
+
+	SET_BIT(ADCSRA,ADSC); /* Start conversion write '1' to ADSC */
+
+	while(BIT_IS_CLEAR(ADCSRA,ADIF)); /* Wait for conversion to complete, ADIF becomes '1' */
+
+
+	SET_BIT(ADCSRA,ADIF); /* Clear ADIF by write '1' to it :) */
+
+
+	return ADC; /* Read the digital value from the data register */
+}
+
+
+/*
+ * Description :
+ * Function responsible for read analog data from a certain ADC channel in case of interrupt mode.
+ * and convert it to digital using the ADC driver.
+ */
+void ADC_readChannel_interrupt(ADMUX_SEL channel_num)
+{
+	channel_num &= 0x07; /* Input channel number must be from 0 --> 7 */
+	ADMUX &= 0xE0; /* Clear first 5 bits in the ADMUX (channel number MUX4:0 bits) before set the required channel */
+	ADMUX = ADMUX | channel_num; /* Choose the correct channel by setting the channel number in MUX4:0 bits */
+	SET_BIT(ADCSRA,ADSC); /* Start conversion write '1' to ADSC */
+}
--- Fan/Fan/adc.h
+++ Fan/Fan/adc.h
@@ -9,8 +9,122 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+#include "std_types.h"
+
+
+#define ADC_MAXIMUM_VALUE     1023
+#define ADC_REF_VOL_VALUE     2.56
+
+
+extern volatile uint16 g_adcResult;
+extern volatile uint8  g_lmsensor;
+
+
+/*
+ * Description :
+ * Select the way VREF is selected
+ * 1.AREF_SEL : VREF is entered throw and external AREF PIN in micro-controller.
+ * 2.AVCC_SEL : VREF is the same as AVCC (5v analog).
+ * 3.INTERNAL_SEL : VREF is 2.56v from internal micro-controller circuit.
+ * Done using (ADMUX) Register.
+ */
+typedef enum{
+	AREF_SEL=0x00 , AVCC_SEL=0x40 , INTERNAL_SEL=0xC0
+}VREF_SEL;
+
+
+/*
+ * Description :
+ * Adjust ADC DATA Register right/left using (ADMUX) Register.
+ */
+typedef enum{
+	ADLAR_RIGHT=0x00 , ADLAR_LEFT=0x20
+}ADLAR_SEL;
+
+
+/*
+ * Description :
+ * Select ADC channel to convert it to digital according to channel number with (ADMUX) Register.
+ */
+typedef enum{
+	ADC0,ADC1,ADC2,ADC3,ADC4,ADC5,ADC6,ADC7
+}ADMUX_SEL;
+
+
+/*
+ * Description :
+ * Enable/Disable ADC using (ADCSRA) Register.
+ */
+typedef enum{
+	ADC_DISABLED,ADC_ENABLED=0x80
+}ADC_ENABLE;
+
+
+/*
+ * Description :
+ * Select ADC channel to work with pooling/interrupt mode with (ADCSRA) Register.
+ */
+typedef enum{
+	POOLING_MODE=0x00 ,INTERRUPT_MODE=0x08
+}ADC_MODE;
+
+
+/*
+ * Description :
+ * Select ADC Auto-Trigger mode(source) to of ADC in (SFIOR) Register.
+ */
+typedef enum{
+	AUTO_TRIGGER_DISABLED,ANALOG_COMP=0x20,EXTERNAL_INT0=0x40,TIMER0_CMP=0x60,TIMER0_OVF=0x80,TIMER1_CMPB=0xA0,TIMER1_OVF=0xC0,TIMERR1_CAPTURE=0xE0
+}ADC_AUTOTRIGGER;
+
+
+/*
+ * Description :
+ * Select ADC clock (F_ADC=F_CPU/Prescaler) with (ADCSRA) Register.
+ */
+typedef enum{
+	FCPU_2,FCPU_4=0x02,FCPU_8=0x03,FCPU_16=0x04,FCPU_32=0x05,FCPU_64=0x06,FCPU_128=0x07
+}ADC_PRESCALER;
+
+
+/*
+ * Description :
+ * Structure variable passed to ADC_init function as parameter in order to initialize the ADC
+ * 1.AREF : VREF is entered throw and external AREF PIN in micro-controller.
+ * 2.AVCC : VREF is the same as AVCC (5v analog).
+ * 3.INTERNAL : VREF is 2.56v from internal micro-controller circuit.
+ */
+typedef struct{
+	VREF_SEL vref;
+	ADLAR_SEL adalr;
+	ADC_ENABLE enable;
+	ADC_MODE mode;
+	ADC_AUTOTRIGGER autotrigger;
+	ADC_PRESCALER prescaler;
+}adc_config;
+
+
+/*
+ * Description :
+ * Function responsible for initialize the ADC driver.
+ */
+void ADC_init(const adc_config * Config_Ptr);
+
+
+/*
+ * Description :
+ * Function responsible for read analog data from a certain ADC channel in case of pooling mode.
+ * and convert it to digital using the ADC driver.
+ */
+uint16 ADC_readChannel_pooling(ADMUX_SEL channel_num);
 
 
 
+/*
+ * Description :
+ * Function responsible for read analog data from a certain ADC channel in case of interrupt mode.
+ * and convert it to digital using the ADC driver.
+ */
+void ADC_readChannel_interrupt(ADMUX_SEL channel_num);
 
 #endif /* ADC_H_ */
