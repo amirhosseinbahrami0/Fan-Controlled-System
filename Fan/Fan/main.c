048e85cd654a8aefe3026a4260e5b959a771904d
AMIR <55827972+amirhosseinbahrami0@users.noreply.github.com>
2024-12-20 11:04:43 +0330

    initial setup

---
+++ Fan/Fan/main.c
@@ -0,0 +1,18 @@
+/*
+ * Fan.c
+ *
+ * Created: 12/20/2024 11:02:08 AM
+ * Author : Amir
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
