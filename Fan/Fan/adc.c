52c625c8b65853b0713dc4aa629a5b65326f829b
AMIR <55827972+amirhosseinbahrami0@users.noreply.github.com>
2024-12-20 12:33:38 +0330

    add ADC file

---
+++ Fan/Fan/adc.c
@@ -0,0 +1,6 @@
+/*
+ * adc.c
+ *
+ * Created: 12/20/2024 12:31:28 PM
+ *  Author: Amir
+ */ 
---
+++ Fan/Fan/adc.h
@@ -0,0 +1,16 @@
+/*
+ * adc.h
+ *
+ * Created: 12/20/2024 12:31:08 PM
+ *  Author: Amir
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+
+
+
+#endif /* ADC_H_ */
